b815630e9bf8b1a618dcc7760aea8c90952e5591
GabrielBlanc0o <gtbvacation@outlook.es>
2026-04-08 13:28:52 -0500

    fixed blankspaces

--- backend/src/main/java/com/gabriel/erp/backend/service/ProductService.java
+++ backend/src/main/java/com/gabriel/erp/backend/service/ProductService.java
@@ -1,7 +1,5 @@
 package com.gabriel.erp.backend.service;
-
 import org.springframework.stereotype.Service;
-
 import com.gabriel.erp.backend.model.Product;
 import com.gabriel.erp.backend.repository.ProductRepository;
 
@@ -12,26 +10,18 @@ public class ProductService {
     //inyect to the repo
     private final ProductRepository productRepository;
 
-
     // CONSTRUCTOR FOR allow to spring inyect the dependence automatic
     public ProductService(ProductRepository productRepository)
     {
         this.productRepository = productRepository;
     }
-
     // an example below
-
     public Product saveProduct(Product product)
     {
         if (product.getPrice()<0){
             throw new RuntimeException("The price cannot be negative");
-
         }
+
         return productRepository.save(product);
-
-
     }
-
-
-
 }
